2cb95b63b7623e75e115d4d79e90e40f9a2d6916
finnes1 <innesfreya@gmail.com>
2026-09-14 13:36:58 -0700

    Changed working directory to the right place and built the filter

--- clean_filter_091126.R
+++ clean_filter_091126.R
@@ -4,11 +4,29 @@
 
 # Open Packages
 library(tidyverse)
-library()
+#library()
 
 #### READING IN DATA ####
-setwd("~/Desktop/School/Graduate/BIMBY 2026 Field Season Code/BIMBY 2026 Code") # set wd
+setwd("~/Desktop/School/Graduate/BIMBY_2026_Field_Season")
 
 butterflies_raw <- read_csv("BIMBY-field-work-butterfly-2026.csv")
 flowers_raw <- read_csv("BIMBY-field-work-flowers-2026.csv")
 nectar_raw <- read_csv("BIMBY-field-work-nectaring-2026.csv")
+
+#### FILTER OUT SURVEYS THAT WERE TOO COLD/WINDY ####
+
+butterflies_filtered <- butterflies_raw %>%
+  select(-starts_with("...")) %>% # Removes junk columns that come with download from Google Sheets
+  mutate(temp_avg = (temp_beg + temp_end)/2,
+         wind_avg = (wind_beg + wind_end)/2,
+         duration_s = time_end - time_beg) %>% # Creates average temperature, average wind, and duration columns
+  filter(wind_avg < 4,
+         temp_avg >= 15,
+         !(weather == "cloudy" & temp_avg < 18)) %>% # Filters for standardised butterfly conditions
+  select(month, day, week, observer, transect, lat, lon, elevation_m, time_beg, duration_s,
+         temp_avg, wind_avg, `Air Quality`, weather, family, genus, species, number) 
+
+ggplot(butterflies_filtered, aes(x = transect, y = duration_s, colour = week)) +
+  geom_point()
+  
+  
