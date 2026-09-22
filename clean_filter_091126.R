98cbf3d7162d91b4f9630b68c223a9026851577d
finnes1 <innesfreya@gmail.com>
2026-09-22 15:46:56 -0700

    Making new folder for clean data and adding semi-clean data to that folder

--- clean_filter_091126.R
+++ clean_filter_091126.R
@@ -30,6 +30,8 @@ ggplot(butterflies_filtered, aes(x = transect, y = duration_s, colour = week)) +
   theme_bw() # checking time data is correctly inputted
 
 
-
-
+#### WRITING CLEANED DATA INTO FILE ####
+write.csv(butterflies_filtered,"Clean Data/butterflies_clean.csv", row.names = FALSE)
+write.csv(flowers_raw,"Clean Data/flowers_clean.csv", row.names = FALSE) # PLACE HOLDER SINCE THESE HAVE NOT BEEN CLEANED YET
+write.csv(nectar_raw,"Clean Data/nectaring_clean.csv", row.names = FALSE) # PLACE HOLDER SINCE THESE HAVE NOT BEEN CLEANED YET
 
